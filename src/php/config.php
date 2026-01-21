941fefaf9485d8ae01d1164c7b8c20f279c0b470
Rhaastrake <garofalomichele2001@gmail.com>
2026-01-21 15:01:31 +0100

    Added _site in girignore to hide config php and added configExample.php to github

--- src/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configurazione SMTP
-define('MAIL_USERNAME', 'garofalomichele2001@gmail.com'); // account Gmail
-define('MAIL_PASSWORD', 'fqxtgpnoreqilfbf');               // App Password Gmail
-define('MAIL_HOST', 'smtp.gmail.com');                     // host SMTP
-define('MAIL_PORT', 587);                                  // porta SMTP
-define('MAIL_FROM_NAME', 'MicheleGaro.it');                // nome mittente
-?>
---
+++ src/php/configExample.php
@@ -0,0 +1,8 @@
+<?php
+// Configurazione SMTP
+define('MAIL_USERNAME', '? ? ?'); // account Gmail
+define('MAIL_PASSWORD', '? ? ?');               // App Password Gmail
+define('MAIL_HOST', 'smtp.gmail.com');                     // host SMTP
+define('MAIL_PORT', 587);                                  // porta SMTP
+define('MAIL_FROM_NAME', '? ? ?');                // nome mittente
+?>
