46f2e137d0b2a2307fd8ca001df0405ed22d9e95
Rhaastrake <garofalomichele2001@gmail.com>
2026-02-17 11:43:39 +0100

    Added keywords to pages

---
+++ src/_data/site.js
@@ -0,0 +1,30 @@
+const { title } = require("../../../roberta/website_dottoressa_tene/src/_data/site");
+
+module.exports = {
+  homepage: {
+    seo: {
+      title: "Webfixer - Homepage",
+      description: "Assistenza tecnica per PC e sistemi informatici.",
+      keywords: "Riparazione computer Bari, computer rotto bari, computer lento, pulizia computer bari, negozio computer bari, computer si spegne da solo, michele garofalo, tecnico pc bari",
+    },
+  },
+  about: {
+    seo: {
+      title: "Webfixer - Curriculum",
+      description: "Assistenza tecnica per PC e sistemi informatici.",
+      keywords: "michele garofalo, programmatore, tenico pc, curriculum, chi sono",
+    },
+  },
+  news: {
+    seo: {
+      title: "Webfixer - Notizie",
+      description: "",
+      keywords: "",
+    },
+  },
+  404: {
+    seo: {
+        title: "Webfixer - 404"
+    }
+  }
+};
